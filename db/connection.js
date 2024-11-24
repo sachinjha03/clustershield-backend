19f01d01295d4bd27053c6d9f6862b6797630e13
Sachin Jha <139990682+sachinjha03@users.noreply.github.com>
2024-11-24 22:53:59 +0530

    Update connection.js

--- db/connection.js
+++ db/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-mongoose.connect("mongodb://127.0.0.1:27017/clusterShield").then(() => {
+mongoose.connect("mongodb+srv://sachin03nic:sachin03nic@clustershield.zqdlo.mongodb.net/clustershield?retryWrites=true&w=majority&appName=clustershield").then(() => {
     console.log("Conencting to the database....");
 }).catch((err) => {
     console.log("Failed to connect , Reason :- ", err);
